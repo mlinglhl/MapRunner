fd594a584f2de27b351e7bf33ca82584e4cbffd6
mlinglhl <minhung.ling@gmail.com>
2017-04-05 22:27:36 -0400

    The map shows up at the endd now

---
+++ MapRunner/ResultsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ResultsViewController.swift
+//  MapRunner
+//
+//  Created by Minhung Ling on 2017-04-05.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ResultsViewController: UIViewController, MKMapViewDelegate {
+    var locations = [CLLocationCoordinate2D]()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        mapView.mapType = .standard
+//        mapView.showsUserLocation = true
+        let spanX = 0.007
+        let spanY = 0.007
+        if locations.count > 0 {
+            let region = MKCoordinateRegion(center: locations.last!, span: MKCoordinateSpanMake(spanX, spanY))
+            mapView.setRegion(region, animated: false)
+            drawPath()
+        }
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    }
+    
+    func drawPath() {
+        var pathArray = [CLLocationCoordinate2D]()
+        for index in 0..<locations.count {
+            pathArray.append(locations[index])
+        }
+        let polyline = MKPolyline(coordinates: pathArray, count: pathArray.count)
+        mapView.add(polyline)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+        polylineRenderer.strokeColor = UIColor.blue
+        polylineRenderer.lineWidth = 4
+        return polylineRenderer
+    }
+    
+}
---
+++ MapRunner/RunViewController.swift
@@ -0,0 +1,54 @@
+//
+//  RunViewController.swift
+//  MapRunner
+//
+//  Created by Minhung Ling on 2017-04-05.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+//
+//  HomeViewController.swift
+//  MapRunner
+//
+//  Created by Minhung Ling on 2017-04-04.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class RunViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var myLocations = [CLLocationCoordinate2D]()
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count > 0 {
+            let location = locations.last!
+            myLocations.append(location.coordinate)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ResultsViewController" {
+            let rvc = segue.destination as! ResultsViewController
+            rvc.locations = myLocations
+        }
+    }
+}
